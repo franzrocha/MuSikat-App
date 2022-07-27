fd75a94e8586e27903e134a25e0666ee7fe09267
Franz Lesly Rocha <franzlesly.rocha.20@usjr.edu.ph>
2022-07-28 02:37:51 +0800

    welcome screen updated

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -26,20 +26,72 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Image.asset("assets/images/musikat_logo.png",
-                    width: 141, height: 141),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Image.asset("assets/images/musikat_logo.png",
+                      width: 141, height: 141),
+                ),
                 Text("MuSikat",
                     style: GoogleFonts.montserrat(
                         color: Colors.white,
                         fontSize: 36,
                         fontWeight: FontWeight.bold)),
-                    Text("LISTEN TO YOUR FAVOURITE \n OPM TRACKS HERE ",
+                Text("LISTEN TO YOUR FAVOURITE \n OPM TRACKS HERE ",
                     textAlign: TextAlign.center,
                     style: GoogleFonts.inter(
                         color: Colors.white,
                         fontSize: 20,
                         fontWeight: FontWeight.w500)),
-                        
+                Container(
+                  width: 318,
+                  height: 63,
+                  decoration: BoxDecoration(
+                      color: const Color(0xfffca311),
+                      borderRadius: BorderRadius.circular(60)),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'LOG IN',
+                      style: GoogleFonts.montserrat(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 318,
+                  height: 63,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff34b771),
+                      borderRadius: BorderRadius.circular(60)),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'SIGN UP',
+                      style: GoogleFonts.montserrat(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                Text("or connect with",
+                    style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500)),
+                const Divider(
+                    color: Color(0xff707579),
+                    thickness: 0.4,
+                    indent: 50,
+                    endIndent: 50,
+                    height: 0.5),
+                Text("or connect with",
+                    style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500)),
               ],
             ),
           ),
