f8fe37dd8a3459a7b919bce389723a5e2cfee221
Franz Lesly Rocha <franzlesly.rocha.20@usjr.edu.ph>
2022-08-05 03:19:46 +0800

    initial register screen

--- lib/screens/authentication/register_screen.dart
+++ lib/screens/authentication/register_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:musikat_app/controllers/auth_controller.dart';
+import 'package:musikat_app/service_locators.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -8,12 +12,68 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailCon = TextEditingController(),
+      _passCon = TextEditingController(),
+      _pass2Con = TextEditingController(),
+      _usernameCon = TextEditingController(),
+      _ageCon = TextEditingController();
+
+  final genderList = ["Prefer not to say", "Male", "Female", "Others"];
+  String dropdownValue = 'Prefer not to say';
+
+  final AuthController _authController = locator<AuthController>();
+
+  String prompts = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: appBar(context),
+      backgroundColor: const Color(0xff262525),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      "assets/images/musikat_logo.png",
+                      width: 50,
+                      height: 50,
+                    ),
+                    Text("MuSikat",
+                        style: GoogleFonts.montserrat(
+                            color: Colors.white,
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-
-    )
-    ;
+  AppBar appBar(BuildContext context) {
+    return AppBar(
+      toolbarHeight: 75,
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      automaticallyImplyLeading: false,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: const FaIcon(
+          FontAwesomeIcons.angleLeft,
+          size: 20,
+        ),
+      ),
+    );
   }
 }
