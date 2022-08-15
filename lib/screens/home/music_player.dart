eef25edd3cf8767622d2e6e5eb98be0e6509e2b0
Franz Lesly Rocha <franzlesly.rocha.20@usjr.edu.ph>
2022-08-16 03:44:51 +0800

    removed some unnecessary widgets

--- lib/screens/home/music_player.dart
+++ lib/screens/home/music_player.dart
@@ -5,9 +5,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:musikat_app/constants.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
-  const MusicPlayerScreen({Key? key, required this.title}) : super(key: key);
+  const MusicPlayerScreen({Key? key}) : super(key: key);
 
-  final String title;
 
   @override
   State<MusicPlayerScreen> createState() => _MusicPlayerScreenState();
@@ -74,17 +73,16 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     return Scaffold(
         appBar: appbar(context),
         backgroundColor: musikatBackgroundColor,
-        body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Column(
+        body: SafeArea(
+          child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
                     children: [
                       Container(
-                        width: 300,
+                        width: 310,
                         height: 300,
                         decoration: BoxDecoration(
                           color: Colors.blue,
@@ -101,167 +99,162 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 35),
-                  child: Row(
-                    children: const [
-                      Text(
-                        "where do we go now?",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                            color: Colors.orange),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35),
+                    child: Row(
+                      children: const [
+                        Text(
+                          "where do we go now?",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: Colors.orange),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35),
+                    child: Row(
+                      children: const [
+                        Text(
+                          "Desiree Armojallas",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SliderTheme(
+                    data: const SliderThemeData(
+                      thumbColor: buttonColor,
+                      overlayColor: Color.fromRGBO(255, 240, 210, 0.5),
+        
+                    ),
+                    child: Slider(
+                      min: 0,
+                      max: duration.inSeconds.toDouble(),
+                      value: position.inSeconds.toDouble(),
+                      onChanged: (value) async {
+                        final position = Duration(seconds: value.toInt());
+                        await player.seek(position);
+        
+                        await player.resume();
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          time(position),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          time(duration - position),
+                          style: const TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const FaIcon(
+                        FontAwesomeIcons.backwardStep,
+                        size: 35,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(width: 30),
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.topRight,
+                            colors: [
+                              Color(0xfffca311),
+                              Color(0xff62DD69),
+                            ],
+                          ),
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: IconButton(
+                          color: const Color.fromARGB(255, 26, 25, 25),
+                          onPressed: () async {
+                            if (isPlaying) {
+                              await player.pause();
+                            } else {
+                              await player.resume();
+                            }
+                          },
+                          icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+                          iconSize: 50,
+                        ),
+                      ),
+                      const SizedBox(width: 30),
+                      const FaIcon(
+                        FontAwesomeIcons.forwardStep,
+                        size: 35,
+                        color: Colors.white,
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 35),
-                  child: Row(
-                    children: const [
-                      Text(
-                        "Desiree Armojallas",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20,
-                            color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SliderTheme(
-<<<<<<< Updated upstream
-                  data: const SliderThemeData(
-                    thumbColor: buttonColor,
-                    overlayColor: Color.fromRGBO(255, 240, 210, 0.5),
-=======
-                  data: SliderThemeData(
-                    thumbColor: buttonColor,
-                    overlayColor: Color.fromARGB(255, 240, 210, 165),
->>>>>>> Stashed changes
-                  ),
-                  child: Slider(
-                    min: 0,
-                    max: duration.inSeconds.toDouble(),
-                    value: position.inSeconds.toDouble(),
-                    onChanged: (value) async {
-                      final position = Duration(seconds: value.toInt());
-                      await player.seek(position);
-
-                      await player.resume();
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  const SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        time(position),
-                        style: const TextStyle(color: Colors.white),
+                        'LYRICS',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.inter(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
                       ),
+                      const SizedBox(width: 70),
+                      const FaIcon(
+                        FontAwesomeIcons.heart,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(width: 80),
                       Text(
-                        time(duration - position),
-                        style: const TextStyle(color: Colors.white),
-                      )
+                        'INFO',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.inter(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
                     ],
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const FaIcon(
-                      FontAwesomeIcons.backwardStep,
-                      size: 35,
-                      color: Colors.white,
-                    ),
-                    const SizedBox(width: 30),
-                    Container(
-                      decoration: BoxDecoration(
-                        gradient: const LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.topRight,
-                          colors: [
-                            Color(0xfffca311),
-                            Color(0xff62DD69),
-                          ],
-                        ),
-                        border: Border.all(
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      child: IconButton(
-                        color: const Color.fromARGB(255, 26, 25, 25),
-                        onPressed: () async {
-                          if (isPlaying) {
-                            await player.pause();
-                          } else {
-                            await player.resume();
-                          }
-                        },
-                        icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-                        iconSize: 50,
-                      ),
-                    ),
-                    const SizedBox(width: 30),
-                    const FaIcon(
-                      FontAwesomeIcons.forwardStep,
-                      size: 35,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 50),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'LYRICS',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.inter(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(width: 70),
-                    const FaIcon(
-                      FontAwesomeIcons.heart,
-                      color: Colors.white,
-                    ),
-                    const SizedBox(width: 80),
-                    Text(
-                      'INFO',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.inter(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )));
+                ],
+              )),
+        ));
   }
 
   AppBar appbar(BuildContext context) {
