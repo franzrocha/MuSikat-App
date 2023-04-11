15d215e17b81ed92815d22bd21458162fef68d5a
fabian <jekoy99@gmail.com>
2023-04-12 03:45:31 +0800

    added card on mood screen

--- lib/screens/home/categories/mood_screen.dart
+++ lib/screens/home/categories/mood_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:musikat_app/widgets/toast_msg.dart';
 
 import '../../../utils/constants.dart';
 
@@ -13,21 +14,89 @@ class MoodsScreen extends StatefulWidget {
 }
 
 class _MoodsScreenState extends State<MoodsScreen> {
-
+  List<Map<String, dynamic>> roomDataList = [
+    {
+      "name": "Romance",
+      "color": const Color.fromARGB(255, 70, 69, 68),
+    },
+    {
+      "name": "Gaming",
+      "color": const Color.fromARGB(255, 18, 40, 58),
+    },
+    {
+      "name": "Sad",
+      "color": const Color.fromARGB(255, 22, 22, 16),
+    },
+     {
+      "name": "Happy",
+      "color": const Color.fromARGB(255, 68, 18, 23),
+    },
+    {
+      "name": "Relaxing",
+      "color": const Color.fromARGB(255, 182, 111, 118),
+    },
+    {
+      "name": "Studying",
+      "color": const Color.fromARGB(255, 141, 38, 48),
+    },
+  ];
   @override
- Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: appbar(context),
-     backgroundColor: musikatBackgroundColor,
-    
-  );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appbar(context),
+      backgroundColor: musikatBackgroundColor,
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: roomDataList.map((roomData) {
+          final String roomName = roomData['name'];
+          final Color roomColor = roomData['color'];
+          return GestureDetector(
+           onTap: () {                          
+            
+  },                      
+            child: SizedBox(
+              height: 200,
+              child: Container(
+                height: 200,
+                margin: const EdgeInsets.all(16.0),
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: roomColor,
+                  borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      spreadRadius: 0.5,
+                      offset: Offset(2.0, 2.0),
+                      blurRadius: 5.0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(roomName,
+                        style: const TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
 }
 
   AppBar appbar(BuildContext context) {
     return AppBar(
       toolbarHeight: 75,
       title: Text(
-        "Mood",
+        "Moods",
         textAlign: TextAlign.right,
         style: GoogleFonts.inter(
           color: Colors.white,
@@ -49,4 +118,4 @@ class _MoodsScreenState extends State<MoodsScreen> {
       ),
     );
   }
-}
+
